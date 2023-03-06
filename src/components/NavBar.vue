4fac3314ca12da8ea21b5d9c5858a7e925003df1
sudhanshulicensed <sudhanshu.licensed@gmail.com>
2023-03-06 23:15:27 +0530

    Added Rough design for navigation bar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-toolbar class="background text-white shadow-2 rounded-borders">
+    <img src="../assets/Logo/Logo.svg" alt="Page Logo" />
+  </q-toolbar>
+</template>
+
+<script setup>
+// import logo from "../assets/Logo/Logo.svg"
+</script>
+
+<style scoped>
+.navigation {
+  width: 1920px;
+  height: 80px;
+}
+.background {
+  background: #050824;
+  border-radius: 0;
+}
+</style>
+
